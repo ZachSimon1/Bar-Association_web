bf75242d577890dda1e1361c01b2b8bd4db8bb1c
ZachSimon1 <haafrxhhnuidrop@gmail.com>
2025-07-24 14:11:52 +0300

    Create Hero.js

---
+++ src/src/src/src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <h1>עו"ד אלון בר</h1>
+      <p>ייצוג משפטי ברמה הגבוהה ביותר</p>
+      <a href="#contact" className="btn">צור קשר</a>
+    </div>
+  );
+}
+
+export default Hero;
